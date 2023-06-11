d6681a10cf57f6eaddcbee8cadd47a3e7c59ede5
Artur A. Galstyan <63471891+Artur-Galstyan@users.noreply.github.com>
2023-06-11 11:47:29 +0200

    built to onmount

--- frontend/src/routes/datasets/[db]/+page.svelte
+++ frontend/src/routes/datasets/[db]/+page.svelte
@@ -1,26 +1,64 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { getGitHubFilesInFolder, getRawGitHubContent } from '$lib/utils/githubUrlBuilder';
+	import { marked } from 'marked';
+	import { onMount } from 'svelte';
 
-    import {page} from "$app/stores"
-	
-    console.log($page.url)
+	let content: string | undefined;
+	let files: string[];
+
+	async function load(params: any) {
+		try {
+			const githubUrl = getRawGitHubContent(`Artur-Galstyan/leaderboard`, `${params.db}/README.md`);
+
+			const githubMarkdownTextReq = await fetch(githubUrl);
+			const githubMarkdownText = await githubMarkdownTextReq.text();
+
+			const htmlContent = marked.parse(githubMarkdownText, { mangle: false, headerIds: false });
+
+			const rawFilesUrl = getGitHubFilesInFolder(`leaderboard`, `${params.db}`);
+			const rawFilesReq = await fetch(rawFilesUrl);
+			const rawFiles = await rawFilesReq.json();
+
+			const files = rawFiles.map((file: { name: string }) => {
+				file.name = file.name.replace('.md', '');
+				return file.name == 'README' ? undefined : file.name;
+			});
+
+			return {
+				content: htmlContent,
+				files: files
+			};
+		} catch (e) {
+			return {
+				status: 404
+			};
+		}
+	}
+
+	onMount(async () => {
+		const loadData = await load($page.params);
+		content = loadData.content;
+		files = loadData.files;
+	});
 </script>
 
-<article class="prose text-justify mx-auto my-8">
+{#if content}
+	<article class="prose text-justify mx-auto my-8">
+		{@html content}
+	</article>
+{/if}
 
-    {@html $page.data.content}
-
-</article>
-
-
-<div class="flex justify-center">
-
-    <ul class="list list-decimal">
-        {#each $page.data.files as file}
-            {#if file}
-                <li>
-                    <a href={$page.url.pathname + "/"  + file}>{file}</a>
-                </li>
-            {/if}
-        {/each} 
-    </ul>
-</div>
+{#if files}
+	<div class="flex justify-center">
+		<ul class="list list-decimal">
+			{#each files as file}
+				{#if file}
+					<li>
+						<a href={$page.url.pathname + '/' + file}>{file}</a>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	</div>
+{/if}
--- frontend/src/routes/datasets/[db]/[dataset]/+page.svelte
+++ frontend/src/routes/datasets/[db]/[dataset]/+page.svelte
@@ -1,13 +1,56 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { getRawGitHubContent } from '$lib/utils/githubUrlBuilder';
+	import matter from 'gray-matter';
+	import * as htmlToJson from 'html-table-to-json';
+	import { marked } from 'marked';
 	import { onMount } from 'svelte';
 	import { TabulatorFull as Tabulator } from 'tabulator-tables';
 
+	let parsedTable: any;
+	let prefaceData: any;
+
+	async function load(params: any) {
+		try {
+			const githubUrl = getRawGitHubContent(
+				`Artur-Galstyan/leaderboard`,
+				`${params.db}/${params.dataset}.md`
+			);
+			const githubUrlReq = await fetch(githubUrl);
+			const githubMarkdownText = await githubUrlReq.text();
+
+			const parsed = matter(githubMarkdownText);
+			const prefaceData = {
+				name: parsed.data.name,
+				description: parsed.data.description,
+				datasetUrl: parsed.data.datasetUrl
+			};
+			const htmlContent = marked.parse(parsed.content, { mangle: false, headerIds: false });
+			const parsedTable = htmlToJson.parse(htmlContent).results[0];
+
+			return {
+				parsedTable: structuredClone(parsedTable),
+				prefaceData: prefaceData
+			};
+		} catch (e) {
+			throw new Error(`Failed to load dataset ${params.dataset} from ${params.db}`);
+		}
+	}
+
 	let table: string | HTMLElement;
-	onMount(() => {
-		console.log($page.data.parsedTable);
+	onMount(async () => {
+		console.log('page', $page);
+		const loadData = await load($page.params);
+		parsedTable = loadData.parsedTable;
+		prefaceData = loadData.prefaceData;
+
+		console.log('parsedTable', parsedTable);
+		console.log('prefaceData', prefaceData);
+
+		console.log('table', parsedTable);
+
 		new Tabulator(table, {
-			data: $page.data.parsedTable,
+			data: parsedTable,
 			layout: 'fitColumns', //fit columns to width of table (optional)
 			height: '500', //height of table (optional)
 			reactiveData: true, //enable data reactivity
@@ -49,14 +92,14 @@
 	});
 </script>
 
-<main class="prose text-justify mx-auto">
-	<h1>Dataset: {$page.data.prefaceData.name}</h1>
-	<h4>Description: {$page.data.prefaceData.description}</h4>
-	<h5>Dataset URL: <a href={$page.data.prefaceData.datasetUrl}>Link</a></h5>
-</main>
-
+{#if prefaceData && parsedTable}
+	<main class="prose text-justify mx-auto">
+		<h1>Dataset: {prefaceData.name}</h1>
+		<h4>Description: {prefaceData.description}</h4>
+		<h5>Dataset URL: <a href={prefaceData.datasetUrl}>Link</a></h5>
+	</main>
+{/if}
 <div bind:this={table} />
-
 <svelte:head>
 	<link
 		href="https://unpkg.com/tabulator-tables@5.5.0/dist/css/tabulator_bootstrap5.min.css"
