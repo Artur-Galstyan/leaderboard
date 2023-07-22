f975c57c92bf31d54ebbed4c15fd5797d31061eb
Artur-Galstyan <Artur-Galstyan@users.noreply.github.com>
2023-07-22 16:09:59 +0000

    deploy: 8d635ae457a138e48c5a325d48fb831bd71d8779

---
+++ _app/immutable/chunks/index.58403619.js
@@ -0,0 +1,1 @@
+import{a3 as n}from"./index.62b51d1e.js";function r(t,{delay:o=0,duration:e=400,easing:a=n}={}){const i=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:a,css:c=>`opacity: ${c*i}`}}export{r as f};
